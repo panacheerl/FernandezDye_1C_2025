ed1d6ee113c7e198a10febb52ef40a6f3ec267b2
panacheerl <kioru.juan@gmail.com>
2025-03-21 13:37:52 -0300

    struct armado

--- firmware/projects/Guia1_ej3/main/Guia1_ej3.c
+++ firmware/projects/Guia1_ej3/main/Guia1_ej3.c
@@ -31,21 +31,22 @@
 #include "freertos/task.h"
 #include "led.h"
 #include "switch.h"
+
 /*==================[macros and definitions]=================================*/
-
-/*==================[internal data definition]===============================*/
-
-/*==================[internal functions declaration]=========================*/
-
-/*==================[external functions definition]==========================*/
-
 struct leds
 {
-      uint8_t mode;     //  ON, OFF, TOGGLE
+      uint8_t mode;     //  ON, OFF, TOGGLE  // 1,0, 2
 	uint8_t n_led;      //  indica el número de led a controlar
 	uint8_t n_ciclos;  // indica la cantidad de ciclos de ncendido/apagado
 	uint16_t periodo;   // indica el tiempo de cada ciclo
 } my_leds; 
+/*==================[internal data definition]===============================*/
+
+/*==================[internal functions declaration]=========================*/
+void leer_programa(struct leds parametro);
+/*==================[external functions definition]==========================*/
+
+
 
 void app_main(void){
 
@@ -57,51 +58,59 @@ void app_main(void){
 	SwitchesInit();
 
 	struct leds programa_1;
-	programa_1.mode = TOGGLE;
+	programa_1.mode = 2;
 	programa_1.n_led = 1;
 	programa_1.n_ciclos = 5; 
 	struct leds programa_2;
-	programa_2.mode = ON;
+	programa_2.mode = 1;
 	programa_2.n_led = 2;
 
-	struct &leds activo;
+	
 
 
 
+	
+	
+}
 
-if(&activo.mode == ON){
-	if (&my_leds.n_led == 1) {
+void leer_programa(struct leds parametro){
+
+
+
+if(parametro.mode == 1){
+	if (parametro.n_led == 1) {
 		LedOn(LED_1);
-	} else if (&my_leds.n_led == 2){
+	} else if (parametro.n_led == 2){
 		LedOn(LED_2);
-	} else if (&my_leds.n_led == 3) {
+	} else if (parametro.n_led == 3) {
 		LedOn(LED_3);
 	}
-} else if (&my_leds.mode == OFF){
-		if (&my_leds.n_led == 1) {
+} else if (parametro.mode == 0){
+		if (parametro.n_led == 1) {
 		LedOff(LED_1);
-	} else if (&my_leds.n_led == 2){
+	} else if (parametro.n_led == 2){
 		LedOff(LED_2);
-	} else if (&my_leds.n_led == 3) {
+	} else if (parametro.n_led == 3) {
 		LedOff(LED_3);
 	}
-} else if (&my_leds.mode == TOOGLE){
+} else if (parametro.mode == 2){
 
-for (int i=0;i<&my_leds.n_ciclos; i++)
+for (int i=0;i<parametro.n_ciclos; i++)
 
-		if (&my_leds.n_led == 1) {
+		if (parametro.n_led == 1) {
 		LedToggle(LED_1);
-	} else if (&my_leds.n_led == 2){
+	} else if (parametro.n_led == 2){
 		LedToggle(LED_2);
-	} else if (&my_leds.n_led == 3) {
+	} else if (parametro.n_led == 3) {
 		LedToggle(LED_3);
 	}
 
 
 }
 
-	
-	
+
 }
+
+
 
 /*==================[end of file]============================================*/
