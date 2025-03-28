09ce14067b6e602fcef5ee8760f52f3b71c4a99c
Panchito <kioru.juan@gmail.com>
2025-03-28 16:36:52 -0300

    agrega ejercicio 3 con foto y ejercicio 5/6

--- firmware/projects/Guia_1_ej5/main/Guia_1_ej5.c
+++ firmware/projects/Guia_1_ej5/main/Guia_1_ej5.c
@@ -39,14 +39,21 @@ typedef struct
 
 /*==================[internal functions declaration]=========================*/
 void convertirBcdAGpio(uint8_t numeroBCD, gpioConf_t *configuracionGpio);
+uint8_t convertToBcdArray(uint32_t data, uint8_t digits, uint8_t *bcd_number);
+
+void datoAPantalla(uint32_t dato, uint8_t digitos, gpioConf_t *configuracionGpioDigito, gpioConf_t *configuracionGpioCifras);
+
 /*==================[external functions definition]==========================*/
 void app_main(void)
 {
 
-	GPIOInit(GPIO_20, 1);
-	GPIOInit(GPIO_21, 1);
-	GPIOInit(GPIO_22, 1);
-	GPIOInit(GPIO_23, 1);
+	GPIOInit(GPIO_20, GPIO_OUTPUT);
+	GPIOInit(GPIO_21, GPIO_OUTPUT);
+	GPIOInit(GPIO_22, GPIO_OUTPUT);
+	GPIOInit(GPIO_23, GPIO_OUTPUT);
+	GPIOInit(GPIO_19, GPIO_OUTPUT);
+	GPIOInit(GPIO_18, GPIO_OUTPUT);
+	GPIOInit(GPIO_9, GPIO_OUTPUT);
 
 	gpioConf_t vector_pines[4];
 	vector_pines[0].pin = GPIO_20;
@@ -54,31 +61,12 @@ void app_main(void)
 	vector_pines[2].pin = GPIO_22;
 	vector_pines[3].pin = GPIO_23;
 
-	convertirBcdAGpio(5, vector_pines);
+	gpioConf_t vector_cifras[3];
+	vector_cifras[0].pin = GPIO_19;
+	vector_cifras[1].pin = GPIO_18;
+	vector_cifras[2].pin = GPIO_9;
 
-	for (int i = 0; i < 4; i++)
-	{
-
-		printf("%d", vector_pines[i].dir);
-		
-	}
-
-	convertirBcdAGpio(9, vector_pines);
-
-	for (int i = 0; i < 4; i++)
-	{
-
-		printf("%d", vector_pines[i].dir);
-	}
-
-	convertirBcdAGpio(3, vector_pines);
-
-	for (int i = 0; i < 4; i++)
-	{
-
-		printf("%d", vector_pines[i].dir);
-	}
-
+	datoAPantalla(325, 3, vector_pines, vector_cifras);
 }
 
 void convertirBcdAGpio(uint8_t numeroBCD, gpioConf_t *configuracionGpio)
@@ -86,35 +74,88 @@ void convertirBcdAGpio(uint8_t numeroBCD, gpioConf_t *configuracionGpio)
 
 	if (numeroBCD & 1)
 	{
-		configuracionGpio[3].dir = 1;
+		GPIOOn(configuracionGpio[3].pin);
+		
 	}
 	else
 	{
-		configuracionGpio[3].dir = 0;
+		
+		GPIOOff(configuracionGpio[3].pin);
 	}
 	if (numeroBCD & 2)
 	{
-		configuracionGpio[2].dir = 1;
+		
+		GPIOOn(configuracionGpio[2].pin);
 	}
 	else
 	{
-		configuracionGpio[2].dir = 0;
+		
+		GPIOOff(configuracionGpio[2].pin);
 	}
 	if (numeroBCD & 4)
 	{
-		configuracionGpio[1].dir = 1;
+		
+		GPIOOn(configuracionGpio[1].pin);
 	}
 	else
 	{
-		configuracionGpio[1].dir = 0;
+		
+		GPIOOff(configuracionGpio[1].pin);
 	}
 	if (numeroBCD & 8)
 	{
-		configuracionGpio[0].dir = 1;
+		
+		GPIOOn(configuracionGpio[0].pin);
 	}
 	else
 	{
-		configuracionGpio[0].dir = 0;
+		
+		GPIOOff(configuracionGpio[0].pin);
+	}
+}
+
+uint8_t convertToBcdArray(uint32_t data, uint8_t digits, uint8_t *bcd_number)
+{
+	// data : dato de 32 bits
+	// digits : numero de digitos
+	// bcd_number : arreglo donde se guarda el dato
+
+	uint8_t aux1 = data;
+	uint8_t aux2;
+	for (int i = 0; i < digits; i++)
+	{
+
+		aux2 = aux1 % 10;
+		bcd_number[i] = aux2;
+		aux1 = aux1 / 10;
+	}
+
+	return 0;
+}
+
+void datoAPantalla(uint32_t dato, uint8_t digitos, gpioConf_t *configuracionGpioDigito, gpioConf_t *configuracionGpioCifras)
+{
+
+	uint8_t numeroBCD[digitos];
+
+	convertToBcdArray(dato, digitos, numeroBCD);
+
+	for (int i = 0; i < digitos; i++)
+	{
+
+		convertirBcdAGpio(numeroBCD[i], configuracionGpioDigito);
+		// for (int j = 0; j < 4; j++)
+		// {
+		// 	if (configuracionGpioDigito[i].dir == 1)
+		// 	{
+		// 		GPIOOn(configuracionGpioDigito[i].pin);
+		// 	} else {
+		// 		GPIOOff(configuracionGpioDigito[i].pin);
+		// 	}
+		// }
+	
+		GPIOOn(configuracionGpioCifras[i].pin);
+		GPIOOff(configuracionGpioCifras[i].pin);
 	}
 }
 
