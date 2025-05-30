5e927e08180c185294469b5943c8d02c10e3435f
Fr4nc1sc0R0d3 <francisco.rode@ingenieria.uner.edu.ar>
2025-05-30 16:27:47 -0300

    fusion proyecto con ejemplo

--- firmware/projects/Proyecto_3/main/Proyecto_3.c
+++ firmware/projects/Proyecto_3/main/Proyecto_3.c
@@ -32,11 +32,20 @@
 #include <timer_mcu.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+
+
+
+#include "led.h"
+#include "neopixel_stripe.h"
+#include "ble_mcu.h"
 /*==================[macros and definitions]=================================*/
 
 #define pulgar 0
 #define indice 1
 #define numero_dedos 2
+
+#define CONFIG_BLINK_PERIOD 500
+#define LED_BT	LED_1
 
 /*==================[internal data definition]===============================*/
 
@@ -63,17 +72,20 @@ void tarea_enviar_datos()
 	{
 		if (enviar_datos == true)
 		{
-
+			char msg[30];
 			ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
-			UartSendString(UART_PC, "$");
+			// UartSendString(UART_PC, "$");
 
-			for (int i = 0; i < numero_dedos; i++)
-			{
-				if (i > 0)
-					UartSendString(UART_PC, " ");
-				UartSendString(UART_PC, (char *)UartItoa(mediciones[i], 10));
-			}
-			UartSendString(UART_PC, ";");
+			// for (int i = 0; i < numero_dedos; i++)
+			// {
+			// 	if (i > 0)
+			// 		UartSendString(UART_PC, " ");
+			// 	UartSendString(UART_PC, (char *)UartItoa(mediciones[i], 10));
+			// }
+			// UartSendString(UART_PC, ";");
+
+			    sprintf(msg, "1: %d, 2: %d\n", mediciones[0], mediciones[1]);
+    		BleSendString(msg);
 		}
 	}
 }
@@ -96,6 +108,46 @@ void medir_dedos()
 void toggle_enviar_datos()
 {
 }
+
+// void read_data(uint8_t * data, uint8_t length){
+// 	uint8_t i = 1;
+//     static uint8_t red = 0, green = 0, blue = 0;
+// 	char msg[30];
+
+// 	if(data[0] == 'R'){
+//         /* El slidebar Rojo envía los datos con el formato "R" + value + "A" */
+// 		red = 0;
+// 		while(data[i] != 'A'){
+//             /* Convertir el valor ASCII a un valor entero */
+// 			red = red * 10;
+// 			red = red + (data[i] - '0');
+// 			i++;
+// 		}
+// 	}else if(data[0] == 'G'){   
+//         /* El slidebar Verde envía los datos con el formato "G" + value + "A" */
+// 		green = 0;
+// 		while(data[i] != 'A'){
+//             /* Convertir el valor ASCII a un valor entero */
+// 			green = green * 10;
+// 			green = green + (data[i] - '0');
+// 			i++;
+// 		}
+// 	}else if(data[0] == 'B'){
+//         /* El slidebar Azul envía los datos con el formato "B" + value + "A" */
+// 		blue = 0;
+// 		while(data[i] != 'A'){
+//             /* Convertir el valor ASCII a un valor entero */
+// 			blue = blue * 10;
+// 			blue = blue + (data[i] - '0');
+// 			i++;
+// 		}
+// 	}
+//     NeoPixelAllColor(NeoPixelRgb2Color(red, green, blue));
+//     /* Se envía una realimentación de los valores actuales de brillo del LED */
+//     sprintf(msg, "R: %d, G: %d, B: %d\n", red, green, blue);
+//     BleSendString(msg);
+// }
+
 /*==================[external functions definition]==========================*/
 void app_main(void)
 {
@@ -128,6 +180,8 @@ void app_main(void)
 		.sample_frec = 0,
 		.param_p = NULL};
 
+		
+
 	TimerInit(&timer_medicion);
 
 	AnalogInputInit(&input_dedo_1);
@@ -135,9 +189,39 @@ void app_main(void)
 
 	UartInit(&mi_uart);
 	xTaskCreate(&medir_dedos, "medir", 2048, NULL, 5, &timer_medicion_handle);
-	xTaskCreate(&tarea_enviar_datos, "UART", 1024, &mi_uart, 5, &uart_tarea_handle);
+	//xTaskCreate(&tarea_enviar_datos, "UART", 1024, &mi_uart, 5, &uart_tarea_handle);
 
 	TimerStart(timer_medicion.timer);
+
+
+
+
+
+ 
+    ble_config_t ble_configuration = {
+        "ESP_Pancho",
+        BLE_NO_INT
+    };
+
+    LedsInit();
+    BleInit(&ble_configuration);
+ 
+    while(1){
+        vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
+        switch(BleStatus()){
+            case BLE_OFF:
+                LedOff(LED_BT);
+            break;
+            case BLE_DISCONNECTED:
+                LedToggle(LED_BT);
+            break;
+            case BLE_CONNECTED:
+                LedOn(LED_BT);
+            break;
+        }
+    }
+
+
 
 }
 /*==================[end of file]============================================*/
