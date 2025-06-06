dc9260948053b415d249fe5b118e936777587ebc
Fr4nc1sc0R0d3 <francisco.rode@ingenieria.uner.edu.ar>
2025-06-06 16:30:41 -0300

    calibracion rota

--- firmware/projects/Proyecto_3/main/Proyecto_3.c
+++ firmware/projects/Proyecto_3/main/Proyecto_3.c
@@ -32,11 +32,8 @@
 #include <timer_mcu.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-
-
-
+#include "switch.h"
 #include "led.h"
-#include "neopixel_stripe.h"
 #include "ble_mcu.h"
 /*==================[macros and definitions]=================================*/
 
@@ -45,12 +42,13 @@
 #define numero_dedos 2
 
 #define CONFIG_BLINK_PERIOD 500
-#define LED_BT	LED_1
+#define LED_BT LED_1
 
 /*==================[internal data definition]===============================*/
 
 bool enviar_datos = true;
-uint16_t maxima_extension = 3300;
+uint16_t maxima_extension[numero_dedos];
+uint16_t minima_extension[numero_dedos];
 uint16_t mediciones[numero_dedos];
 adc_ch_t canal = 1;
 
@@ -69,27 +67,51 @@ void tarea_enviar_datos()
 {
 
 	while (1)
-	{ 
+	{
 		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
 
 		if (enviar_datos == true)
 		{
 			char msg[30];
-			
-			// UartSendString(UART_PC, "$");
 
-			// for (int i = 0; i < numero_dedos; i++)
-			// {
-			// 	if (i > 0)
-			// 		UartSendString(UART_PC, " ");
-			// 	UartSendString(UART_PC, (char *)UartItoa(mediciones[i], 10));
-			// }
-			// UartSendString(UART_PC, ";");
+			uint32_t porcentajes[numero_dedos];
+			uint16_t salida[numero_dedos];
 
-			    sprintf(msg, "*I%d* *M%d*\n", mediciones[0], mediciones[1]);
-    		BleSendString(msg);
+			for (int i = 0; i < numero_dedos; i++)
+			{
+				porcentajes[i] = (mediciones[i] * 100);
+				salida[i] = porcentajes[i] / maxima_extension[i];
+			}
+
+			sprintf(msg, "*I%d* *M%d*\n", salida[0], salida[1]);
+			BleSendString(msg);
 		}
 	}
+}
+
+void calibrarMax()
+{
+	LedToggle(LED_3);
+	vTaskDelay(50000 / portTICK_PERIOD_MS);
+
+	for (int i = 0; i < numero_dedos; i++)
+	{
+		adc_ch_t canal = i;
+		AnalogInputReadSingle(canal, &maxima_extension[i]);
+	}
+	LedToggle(LED_3);
+}
+
+void calibrarMin()
+{
+	LedToggle(LED_3);
+	vTaskDelay(50000 / portTICK_PERIOD_MS);
+	for (int i = 0; i < numero_dedos; i++)
+	{
+		adc_ch_t canal = i;
+		AnalogInputReadSingle(canal, &minima_extension[i]);
+	}
+	LedToggle(LED_3);
 }
 
 void medir_dedos()
@@ -109,64 +131,22 @@ void medir_dedos()
 
 void toggle_enviar_datos()
 {
+
+	if (enviar_datos == true)
+		enviar_datos = false;
+	else
+		enviar_datos = true;
 }
-
-// void read_data(uint8_t * data, uint8_t length){
-// 	uint8_t i = 1;
-//     static uint8_t red = 0, green = 0, blue = 0;
-// 	char msg[30];
-
-// 	if(data[0] == 'R'){
-//         /* El slidebar Rojo envía los datos con el formato "R" + value + "A" */
-// 		red = 0;
-// 		while(data[i] != 'A'){
-//             /* Convertir el valor ASCII a un valor entero */
-// 			red = red * 10;
-// 			red = red + (data[i] - '0');
-// 			i++;
-// 		}
-// 	}else if(data[0] == 'G'){   
-//         /* El slidebar Verde envía los datos con el formato "G" + value + "A" */
-// 		green = 0;
-// 		while(data[i] != 'A'){
-//             /* Convertir el valor ASCII a un valor entero */
-// 			green = green * 10;
-// 			green = green + (data[i] - '0');
-// 			i++;
-// 		}
-// 	}else if(data[0] == 'B'){
-//         /* El slidebar Azul envía los datos con el formato "B" + value + "A" */
-// 		blue = 0;
-// 		while(data[i] != 'A'){
-//             /* Convertir el valor ASCII a un valor entero */
-// 			blue = blue * 10;
-// 			blue = blue + (data[i] - '0');
-// 			i++;
-// 		}
-// 	}
-//     NeoPixelAllColor(NeoPixelRgb2Color(red, green, blue));
-//     /* Se envía una realimentación de los valores actuales de brillo del LED */
-//     sprintf(msg, "R: %d, G: %d, B: %d pan\n ", red, green, blue);
-//     BleSendString(msg);
-// }
 
 /*==================[external functions definition]==========================*/
 void app_main(void)
 {
-
-	// serial_config_t mi_uart = {
-	// 	.port = UART_PC,
-	// 	.baud_rate = 9600,
-	// 	.func_p = NULL,
-	// 	.param_p = NULL};
 
 	timer_config_t timer_medicion = {
 		.func_p = notificacion_medir,
 		.param_p = NULL,
 		.timer = TIMER_A,
 		.period = 100000};
-
-
 
 	analog_input_config_t input_dedo_1 = {
 		.func_p = NULL,
@@ -182,48 +162,41 @@ void app_main(void)
 		.sample_frec = 0,
 		.param_p = NULL};
 
-		
+	ble_config_t ble_configuration = {
+		"ESP_J_F",
+		BLE_NO_INT};
 
 	TimerInit(&timer_medicion);
 
 	AnalogInputInit(&input_dedo_1);
 	AnalogInputInit(&input_dedo_2);
+	BleInit(&ble_configuration);
+	LedsInit();
+	SwitchesInit();
 
-	//UartInit(&mi_uart);
 	xTaskCreate(&medir_dedos, "medir", 2048, NULL, 5, &timer_medicion_handle);
 	xTaskCreate(&tarea_enviar_datos, "DATOS", 2048, NULL, 5, &enviar_datos_tarea_handle);
+	SwitchActivInt((SWITCH_1 | SWITCH_2), toggle_enviar_datos, NULL);
+	SwitchActivInt(SWITCH_1, calibrarMax, NULL);
+	SwitchActivInt(SWITCH_1, calibrarMin, NULL);
 
 	TimerStart(timer_medicion.timer);
 
-
-
-
-
- 
-    ble_config_t ble_configuration = {
-        "ESP_J_F",
-        BLE_NO_INT
-    };
-
-    LedsInit();
-    BleInit(&ble_configuration);
- 
-    while(1){
-        vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
-        switch(BleStatus()){
-            case BLE_OFF:
-                LedOff(LED_BT);
-            break;
-            case BLE_DISCONNECTED:
-                LedToggle(LED_BT);
-            break;
-            case BLE_CONNECTED:
-                LedOn(LED_BT);
-            break;
-        }
-    }
-
-
-
+	// while (1)
+	// {
+	// 	vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
+	// 	switch (BleStatus())
+	// 	{
+	// 	case BLE_OFF:
+	// 		LedOff(LED_BT);
+	// 		break;
+	// 	case BLE_DISCONNECTED:
+	// 		LedToggle(LED_BT);
+	// 		break;
+	// 	case BLE_CONNECTED:
+	// 		LedOn(LED_BT);
+	// 		break;
+	// 	}
+	// }
 }
 /*==================[end of file]============================================*/
